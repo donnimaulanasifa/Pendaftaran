16e4e899dcac6ffc239d2a1a12672429de43fc8e
Donni Maulana Sifa <mdoni12@gmail.com>
2022-01-24 20:49:03 +0700

    config changes

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,10 +1,6 @@
-const readPkgUp = require('read-pkg-up');
-
-const pkgjson = (readPkgUp.sync()).pkg;
-
 module.exports = {
-	title: pkgjson.title,
-	description: pkgjson.description,
+	title: "Pendaftaran by Donni",
+	description: "Panduan Penggunaan",
 	base: '/Pendaftaran/',
 	themeConfig: {
 		search: true,
