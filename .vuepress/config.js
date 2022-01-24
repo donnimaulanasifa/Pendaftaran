fa3be964f3449fcb2d7804efa61a6304ddc3b136
Donni Maulana Sifa <mdoni12@gmail.com>
2022-01-24 22:10:51 +0700

    try

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,15 +1,46 @@
 const { description } = require('../package')
 
 module.exports = {
-  title: 'Vuepress Docs Boilerplate',
+  title: 'Pendaftaran by Donni',
   description: description,
   base: '/Pendaftaran/',
 
+  watch: {
+    $page(newPage, oldPage) {
+      if (newPage.key !== oldPage.key) {
+        requestAnimationFrame(() => {
+          if (this.$route.hash) {
+            const element = document.getElementById(this.$route.hash.slice(1));
+
+            if (element && element.scrollIntoView) {
+              element.scrollIntoView();
+            }
+          }
+        });
+      }
+    }
+  },
+
+  markdown: {
+  extendMarkdown: md => {
+    md.use(require('markdown-it-multimd-table'), {
+      rowspan: true,
+    });
+  }
+  },
+  
+  
   head: [
     ['meta', { name: 'theme-color', content: '#3eaf7c' }],
     ['meta', { name: 'apple-mobile-web-app-capable', content: 'yes' }],
     ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: 'black' }]
   ],
+  
+  theme: 'vuepress-theme-succinct',
+    globalUIComponents: [
+        'ThemeManager'
+  ],
+  
   themeConfig: {
     repo: '',
     editLinks: false,
@@ -47,8 +78,15 @@ module.exports = {
   /**
    * Apply plugins，ref：https://v1.vuepress.vuejs.org/zh/plugin/
    */
-  plugins: [
+   plugins: [
     '@vuepress/plugin-back-to-top',
-    '@vuepress/plugin-medium-zoom',
-  ]
+    'vuepress-plugin-smooth-scroll',
+    ['vuepress-plugin-medium-zoom',
+        {
+            selector: "img",
+            options: {
+                background: 'var(--bodyBgColor)'
+            }
+        }],
+    ]
 }
